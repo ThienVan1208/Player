9e7e036680798319d401d6cb59e8875f2d53b5db
trannhatdang <77575859+trannhatdang@users.noreply.github.com>
2024-04-22 11:58:29 +0700

    Pause Screen, Death Screen and Point Counter

--- Assets/Scripts/FinalPointCounter.cs
+++ Assets/Scripts/FinalPointCounter.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class UI : MonoBehaviour
+public class FinalPointCounter : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI tmp;
+    public PlayerUI pui;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tmp = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tmp.text = "Your Points: " + pui.point.ToString();
     }
 }
--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DisableRB : MonoBehaviour
+public class StartScreen : MonoBehaviour
 {
-    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.Sleep();
+        
     }
 
     // Update is called once per frame
